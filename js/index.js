d3ef460c28e0a257d3e8a50bac0a0d373abcdc96
Gerasin Ivan <webartmotor@gmail.com>
2020-04-28 19:26:24 +0300

    add visualizer

--- js/index.js
+++ js/index.js
@@ -53,6 +53,26 @@ function clickSettingsButton() {
   });
 }
 
+function clickVisualizerButton() {
+  document.body.addEventListener("click", (e) => {
+    let { target: node } = event;
+    let buttonVisualizer = null;
+
+    while (node !== document.body) {
+      if (node.classList.contains("player__visualizer-button")) {
+        buttonVisualizer = node;
+      }
+
+      if (node.classList.contains("player") && buttonVisualizer) {
+        node.classList.toggle("player_show-visualizer");
+        break;
+      }
+
+      node = node.parentNode;
+    }
+  });
+}
+
 function initContrastAndBrightness() {
   const players = [...document.querySelectorAll(".player")];
 
@@ -133,6 +153,60 @@ function keyDownEsc() {
   });
 }
 
+function visualizer(playerNode) {
+  const AudioContext = window.AudioContext || window.webkitAudioContext;
+  if (!AudioContext) console.error("AudioContext not work");
+  let audioCtx = null;
+  let analyser = null;
+
+  const video = playerNode.querySelector(".player__video");
+  const visualizer = playerNode.querySelector(".player__visualizer");
+  const canvasVisualizer = visualizer.getContext("2d");
+
+  video.addEventListener("volumechange", () => {
+    if (audioCtx) return;
+
+    audioCtx = new AudioContext();
+    analyser = audioCtx.createAnalyser();
+    analyser.fftSize = 1024;
+    const source = audioCtx.createMediaElementSource(video);
+    source.connect(analyser);
+    analyser.connect(audioCtx.destination);
+    audioCtx.resume();
+    const { width, height } = visualizer.getBoundingClientRect();
+
+    const draw = () => {
+      canvasVisualizer.clearRect(0, 0, width, height);
+
+      requestAnimationFrame(draw);
+      const bufferLength = analyser.frequencyBinCount;
+      const dataArray = new Uint8Array(bufferLength);
+      analyser.getByteFrequencyData(dataArray);
+
+      let barWidth = 2;
+      let x = 0;
+      const countBars = Math.floor(width / 3);
+
+      for (let i = 0; i < countBars; i++) {
+        canvasVisualizer.fillStyle = "rgb(100, 149, 237)";
+        canvasVisualizer.fillRect(x, 0, barWidth, dataArray[i] / 3);
+
+        x += barWidth + 1;
+      }
+    };
+
+    draw();
+  });
+}
+
+function renderVisualizers() {
+  const players = [...document.querySelectorAll(".player")];
+
+  players.forEach((player) => {
+    visualizer(player);
+  });
+}
+
 function init() {
   initVideos();
   initToggleButtons();
@@ -141,6 +215,8 @@ function init() {
   clickFullScreen();
   setPlayersStyle();
   keyDownEsc();
+  clickVisualizerButton();
+  renderVisualizers();
 }
 
 init();
